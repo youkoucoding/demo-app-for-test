83fe92387932bc3cdeba8489d40a1a8c0cc711f0
youkou <youkousai@gmail.com>
2022-09-13 16:56:32 +0900

    fetch data

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,19 @@
-var express = require('express');
+var axios = require("axios");
+var express = require("express");
 var router = express.Router();
 
+// function to get the data from the API
+let getData = async () => {
+  let response = await axios.get(`https://catfact.ninja/fact`);
+  return response;
+};
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const users = [
-  { id: 1, name: "john" },
-  { id: 2, name: "kevin" },
-  { id: 3, name: "yama" },
-];
-  res.send({users});
+router.get("/", async function (req, res, next) {
+  let result = await getData();
+  // res.send(result.data.fact);
+
+  res.render("index", { title: result.data.fact });
 });
 
 module.exports = router;
