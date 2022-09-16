8f3e8b9043d8962a41852317f4391b7797a08e43
youkou <youkousai@gmail.com>
2022-09-16 10:39:31 +0900

    function app

---
+++ utils/config/identity.js
@@ -0,0 +1,3 @@
+import { DefaultAzureCredential } from '@azure/identity';
+
+export const credential = new DefaultAzureCredential();
---
+++ utils/cosmosDB/index.js
@@ -0,0 +1,24 @@
+import { CosmosDBManagementClient } from '@azure/arm-cosmosdb';
+import { credential } from '../config/identity';
+import {
+  AZURE_SUBSCRIPTION_ID,
+  AZURE_RESOURCE_GROUP,
+  AZURE_COSMOSDB_ACCOUNT_NAME,
+} from '../config/config';
+
+/**
+ * This sample demonstrates how to Retrieves the properties of an existing Azure Cosmos DB database account.
+ *
+ * @summary Retrieves the properties of an existing Azure Cosmos DB database account.
+ * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2022-08-15/examples/CosmosDBDatabaseAccountGet.json
+ */
+async function cosmosDbDatabaseAccountGet() {
+  const client = new CosmosDBManagementClient(credential, AZURE_SUBSCRIPTION_ID);
+  const result = await client.databaseAccounts.get(
+    AZURE_RESOURCE_GROUP,
+    AZURE_COSMOSDB_ACCOUNT_NAME
+  );
+  console.log(result);
+}
+
+cosmosDbDatabaseAccountGet().catch(console.error);
