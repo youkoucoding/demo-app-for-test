e4d1a43573a3047811092e4659f1e2863d92a2d1
youkou <youkousai@gmail.com>
2022-10-05 18:03:43 +0900

    update

--- items/index.js
+++ items/index.js
@@ -3,20 +3,26 @@ import { ManagedIdentityCredential } from '@azure/identity';
 
 const clientId = process.env.AZURE_CLIENT_ID;
 const authorizedURL = process.env.AUTH_URL;
-
-const credential = new ManagedIdentityCredential(clientId);
+const oauthScope = process.env.API_SCOPE;
+const scope = `${oauthScope}`;
 
 export default async function httpTrigger(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
+  const credential = new ManagedIdentityCredential(clientId);
+  context.log('🚀 ~ file: index.js ~ line 8 ~ credential', credential);
+  try {
+    const token = await credential.getToken(scope);
+    context.log('🚀 ~ file: index.js ~ line 13 ~ httpTrigger ~ token', token);
+  } catch (error) {
+    context.log.error(error);
+  }
 
   const response = await fetch(authorizedURL);
 
   const body = await response.text();
 
-  const token = await credential.getToken();
-
   context.res = {
     status: response.status /* Defaults to 200 */,
-    body: { body, token },
+    body: { body: 'hello world', token: 'hey token' },
   };
 }
