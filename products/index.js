e7c93cb936e55aabcba9003f90ef7704fe3b1713
youkou <youkousai@gmail.com>
2022-10-11 22:42:48 +0900

    fix: fix string

--- products/index.js
+++ products/index.js
@@ -1,12 +1,15 @@
-// import { Cosmos } from './node-cosmos/src';
+import { Cosmos } from '../node-cosmos/dist/index.js';
 
 const dbEndpoint = process.env.AZURE_DB_ENDPOINT;
 const dbKey = process.env.AZURE_DB_KEY;
-const connectionString = ''.concat(...['AccountEndpoint=', dbEndpoint, ';', 'AccountKey=', dbKey]);
+const connectionString = ''.concat(
+  ...['AccountEndpoint=', dbEndpoint, ';', 'AccountKey=', dbKey, ';']
+);
 
-const db = await new Cosmos(connectionString).getDatabase('my-awesome-db');
+const db = await new Cosmos(connectionString).getDatabase('db-name');
 
 export default async function httpTrigger(context, req) {
+  const user1 = await db.read('Collection1', 'id001', 'Users');
   console.log('🚀 ~ file: index.js ~ line 2 ~ httpTrigger ~ req', req);
   context.res = {
     // status: response.status /* Defaults to 200 */,
