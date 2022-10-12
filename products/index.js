2d6dfdf30b934bd3b60beba2d9ac846abc4a24ee
youkou <youkousai@gmail.com>
2022-10-12 13:29:19 +0900

    fix: managed identity success

--- products/index.js
+++ products/index.js
@@ -1,18 +1,29 @@
-import { Cosmos } from '../node-cosmos/dist/index.js';
+import { CosmosClient } from '@azure/cosmos';
+import { ManagedIdentityCredential } from '@azure/identity';
 
 const dbEndpoint = process.env.AZURE_DB_ENDPOINT;
 const dbKey = process.env.AZURE_DB_KEY;
+const clientID = process.env.AZURE_MID;
 const connectionString = ''.concat(
   ...['AccountEndpoint=', dbEndpoint, ';', 'AccountKey=', dbKey, ';']
 );
 
-const db = await new Cosmos(connectionString).getDatabase('db-name');
+const credential = new ManagedIdentityCredential(clientID);
+const aadClient = new CosmosClient({
+  endpoint: dbEndpoint,
+  aadCredentials: credential,
+});
 
 export default async function httpTrigger(context, req) {
-  const user1 = await db.read('Collection1', 'id001', 'Users');
-  console.log('🚀 ~ file: index.js ~ line 2 ~ httpTrigger ~ req', req);
-  context.res = {
-    // status: response.status /* Defaults to 200 */,
-    body: 'products',
-  };
+  try {
+    const res = aadClient.database('<your db name>').container('<container name>').items.readAll();
+    context.res = {
+      // status: response.status /* Defaults to 200 */,
+      body: [res, 'hello ok'],
+    };
+  } catch (error) {
+    context.res = {
+      body: [error, 'hello error'],
+    };
+  }
 }
